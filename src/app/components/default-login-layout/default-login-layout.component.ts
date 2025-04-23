767f9d60e708aeeecac280829093bbb96f8b136f
Gabriel Krigger <gabrikrigger@gmail.com>
2025-04-23 15:37:33 -0300

    feat: creating initial structure

---
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-default-login-layout',
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './default-login-layout.component.html',
+  styleUrl: './default-login-layout.component.scss'
+})
+export class DefaultLoginLayoutComponent {
+
+}
